cc39d609d51136522bc8a36f7ca00dd545cdd057
Asjad <asjadp.19@gmail.com>
2025-08-22 09:07:24 +0000

    add a option ot hide the side bar

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -26,9 +26,7 @@ import { SidebarTrigger } from "./ui/sidebar";
 export function MainHeader() {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-10">
-       <div className="md:hidden">
-        <SidebarTrigger />
-      </div>
+       <SidebarTrigger />
       <div className="w-full flex-1">
         <form>
           <div className="relative">
